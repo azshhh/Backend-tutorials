a589cb3680a15e0410009e48349b386043831d49
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-04 11:53:42 +0530

    48. Handling HTTP GET req.

--- express-tut/index.js
+++ express-tut/index.js
@@ -1,28 +1,25 @@
-const express = require("express");
+const express = require('express');
+const app = express();
 
-const app = express()
+courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' },
+]
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-});
+app.get('/api/courses/hello', (req, res) => {
+    res.send("Hello World");
+})
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3])
-});
+    res.send(courses);
+})
 
 app.get('/api/courses/:id', (req, res) => {
-    res.send(req.params.id)
-});
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send("Course with given ID is not available");
+    res.send(course);
+})
 
-// app.get('/api/posts/:year/:month', (req, res) => {
-//     res.send(req.params)
-// });
-
-// query string params - ?sortBy=name
-app.get('/api/posts/:year/:month', (req, res) => {
-    res.send(req.query)
-});
-
-
-const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`Listensing on port ${port}...`));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
