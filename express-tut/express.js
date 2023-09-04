68de5a0318826462a4c6b1ce8999ce830b0f61b5
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-04 18:27:54 +0530

    Third paty middleware

--- express-tut/express.js
+++ express-tut/express.js
@@ -1,13 +1,16 @@
+const morgan = require('morgan')
+const helmet = require('helmet')
 const Joi = require('joi');
 const logger = require('./logger')
 const express = require('express');
 const app = express();
 
 app.use(express.json());
-// This middleware fxn parses incoming req with urlencoded payloads
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(logger);
+app.use(helmet());
+app.use(morgan('tiny'));
 
 
 courses = [
