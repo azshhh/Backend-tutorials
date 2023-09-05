64973c5c223bbb484920e7240cf91bfc0e5ae6ae
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-05 18:02:10 +0530

    Callback function

--- asynchronous/main.js
+++ asynchronous/main.js
@@ -1,11 +1,16 @@
 console.log('Before');
-const user = getUser(1);
-console.log(user); // this returns 'undefined, bcoz at the time of getUser() is called setTimeout fxn will not be available with object. Since it is delayed for 2 seconds.
+
+// In this the callback fxn is called when the result of async operation is ready.
+getUser(1, (user) => {
+    console.log(user);
+});
+
+// console.log(user); // this returns 'undefined, bcoz at the time of getUser() is called setTimeout fxn will not be available with object. Since it is delayed for 2 seconds.
 console.log('After');
 
-function getUser(id) {
+function getUser(id, callback) {
     setTimeout(() => {
         console.log('Reading user from database...');
-        return { id: id, userName: 'azzy ' }
+        callback({ id: id, userName: 'azzy ' });
     }, 2000);
 }
