606c36d0d87685cf4c1259349661f82c39fcf35d
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-05 18:10:53 +0530

    callback exercise

--- asynchronous/main.js
+++ asynchronous/main.js
@@ -1,16 +1,22 @@
 console.log('Before');
 
-// In this the callback fxn is called when the result of async operation is ready.
 getUser(1, (user) => {
-    console.log(user);
+    getRepositories(user.userName, (repos) => {
+        console.log(repos);
+    }); 
 });
 
-// console.log(user); // this returns 'undefined, bcoz at the time of getUser() is called setTimeout fxn will not be available with object. Since it is delayed for 2 seconds.
 console.log('After');
 
 function getUser(id, callback) {
     setTimeout(() => {
         console.log('Reading user from database...');
         callback({ id: id, userName: 'azzy ' });
+    }, 2000);
+}
+
+function getRepositories(username, callback) {
+    setTimeout(() => {
+        callback (['repo1', 'repo2', 'repo3']);
     }, 2000);
 }
