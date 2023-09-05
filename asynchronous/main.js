527b8b9eebfeb858f243667749457f10ddc15eed
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-05 17:52:41 +0530

    Asynchronous code behaviour 2

--- asynchronous/main.js
+++ asynchronous/main.js
@@ -1,5 +1,11 @@
 console.log('Before');
-setTimeout(() => {
-    console.log('Reading user from database...');
-}, 2000);
+const user = getUser(1);
+console.log(user); // this returns 'undefined, bcoz at the time of getUser() is called setTimeout fxn will not be available with object. Since it is delayed for 2 seconds.
 console.log('After');
+
+function getUser(id) {
+    setTimeout(() => {
+        console.log('Reading user from database...');
+        return { id: id, userName: 'azzy ' }
+    }, 2000);
+}
