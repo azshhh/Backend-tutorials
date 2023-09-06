86f5fc08e9cb99ef8b3bb9dbecc6df77cb52978a
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-06 16:58:33 +0530

    creating a schema

--- mongoDB/index.js
+++ mongoDB/index.js
@@ -3,3 +3,11 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/playground')
     .then(() => console.log('Connected to MongoDB...'))
     .catch(error => console.error('MongoDB connection failed.', error));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean,
+})
