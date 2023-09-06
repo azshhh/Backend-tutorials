2f1eba7be8c1f5afc3651a185ab13f16b4fee596
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-06 17:29:29 +0530

    object for schema compiling Course model

--- mongoDB/index.js
+++ mongoDB/index.js
@@ -1,13 +1,26 @@
 const mongoose = require('mongoose');
 
+// Connecting to MongoDB server
 mongoose.connect('mongodb://localhost/playground')
     .then(() => console.log('Connected to MongoDB...'))
     .catch(error => console.error('MongoDB connection failed.', error));
 
+// Schema structures our document in mDB database.
 const courseSchema = new mongoose.Schema({
     name: String,
     author: String,
     tags: [String],
     date: { type: Date, default: Date.now },
     isPublished: Boolean,
-})
+});
+
+// compiling schema into model which gives a class
+const Course = mongoose.model('Course', courseSchema);
+
+// creating an object specifing values to the schmea holding keys
+const course = new Course({
+    name: 'Node.js Course',
+    author: 'Redbull',
+    tags: ['node', 'backend'],
+    isPublished: true,
+}); 
